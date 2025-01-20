28c327b20c3c9b7cbd5a4a41de55bcfd7c842263
Ashley Lago <ashl3ylag0@gmail.com>
2025-01-19 21:05:16 -0500

    Added the graphs

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,10 +1,12 @@
-// import pieChart from '../../../assets/pie-chart.png';
-// import lineGraph from '../../../assets/line-graph.png';
-// import barGraph from '../../../assets/bar-graph.png';
-// import paperStack from '../../../assets/paper-stack.jpg';
+// Uncommented the imports from assets
+import pieChart from '../../../assets/pie-chart.png';
+import lineGraph from '../../../assets/line-graph.png';
+import barGraph from '../../../assets/bar-graph.png';
+import paperStack from '../../../assets/paper-stack.jpg';
+
 import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
-import {decodeBase64} from '../../../utils/decodeBase64.js';
+// import {decodeBase64} from '../../../utils/decodeBase64.js'; Commented, no longer needed
 
 /**
  * TODO: Ticket 1:
@@ -32,8 +34,40 @@ export const LandingPage = () => {
 
   return (
     <div className='flex-c w-[100vw] secondary-c'>
-      Landing Page
-      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
+      <div data-purpose="Graphs and Charts" className='flex justify-center gap-20 mb-6'>
+        <div className="flex flex-col items-center">
+          <img src={barGraph} alt={"Bar Graph"} className="object-contain w-100 h-60"/>
+          <p className='text-xl'>Search Grant Rates By Office</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={pieChart} alt={"Pie Chart"} className="object-contain w-90 h-60"/>
+          <p className='text-xl'>Search Grant Rates By Nationality</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={lineGraph} alt={"Line Graph"} className="object-contain w-100 h-60"/>
+          <p className='text-xl'>Search Grant Rates Over Time</p>
+        </div>
+      </div>
+      
     </div>
   );
 };
+
+/*
+View the Data
+Download the Data
+
+Human Rights First has created a search tool to give you a user-friendly way to explore a data set of asylum decisions between FY 2016 and May 2021 by the USCIS Asylum Office, which we reviewed through a Freedom of Information Act request. You can search for information on asylum grant rates by year, nationality, and asylum office, visualize the data with charts and heat maps, and download the data set.
+
+System Disparity Insights
+36%
+By the end of the Trump administration, the average asylum office grant rate had fallen 36% from an average of 44 percent in fiscal year 206 to 28 percent in fiscal year 2020.
+5%
+The New York asylum office grant rate dropped to 5 percent in the fiscal year 2020.
+6x Lower
+Between fiscal year 2017 and 2020, the New York asylum office's average grant rate was 6 times lower than the San Francisco asylum office.
+
+Read More
+
+Back to Top ^
+*/
